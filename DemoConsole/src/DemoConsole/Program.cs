8f11451cbd05de897042a8cfb0749a95579e56de
Tauri <tauri139@tlu.ee>
2017-04-26 05:08:02 +0300

    andmebaasi lisamine koodi abil

--- DemoConsole/src/DemoConsole/Program.cs
+++ DemoConsole/src/DemoConsole/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DemoConsole.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,6 +10,20 @@ namespace DemoConsole
     {
         public static void Main(string[] args)
         {
+            using (var db = new ActorDbContext())
+            {
+                db.Actors.AddRange(new Actor { Name = "Bruce Lee", Age = 75, AcademyWinner = false },
+                    new Actor { Name = "Madonna", Age = 58, AcademyWinner = true });
+
+                var count = db.SaveChanges();
+                Console.WriteLine($"{count} records added");
+
+                foreach (var Actor in db.Actors)
+                {
+                    Console.WriteLine($"Name - {Actor.Name},\t\t" + $"Age: {Actor.Age},\t\t" + $"Academy Winner: {Actor.AcademyWinner}");
+                }
+                Console.ReadLine();
+            }
         }
     }
 }
