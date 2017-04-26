21090ab20298f19bf4a41ee8823b007e1d42f5b4
Tauri <tauri139@tlu.ee>
2017-04-26 04:47:48 +0300

    Andmebaas ja tabel lisatud

---
+++ DemoConsole/src/DemoConsole/Migrations/20170426012850_InitialMigration.Designer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using DemoConsole.Entities;
+
+namespace DemoConsole.Migrations
+{
+    [DbContext(typeof(ActorDbContext))]
+    [Migration("20170426012850_InitialMigration")]
+    partial class InitialMigration
+    {
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.1.1")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("DemoConsole.Entities.Actor", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<bool>("AcademyWinner");
+
+                    b.Property<int>("Age");
+
+                    b.Property<string>("Name");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("Actors");
+                });
+        }
+    }
+}
---
+++ DemoConsole/src/DemoConsole/Migrations/ActorDbContextModelSnapshot.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using DemoConsole.Entities;
+
+namespace DemoConsole.Migrations
+{
+    [DbContext(typeof(ActorDbContext))]
+    partial class ActorDbContextModelSnapshot : ModelSnapshot
+    {
+        protected override void BuildModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.1.1")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("DemoConsole.Entities.Actor", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<bool>("AcademyWinner");
+
+                    b.Property<int>("Age");
+
+                    b.Property<string>("Name");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("Actors");
+                });
+        }
+    }
+}
