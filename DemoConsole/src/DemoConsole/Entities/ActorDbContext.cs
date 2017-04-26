ef31ef8d8f819e53e4369b42717f264415b1f387
Tauri <tauri139@tlu.ee>
2017-04-26 04:20:56 +0300

    ActorDbContext klass lisatud.

---
+++ DemoConsole/src/DemoConsole/Entities/ActorDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoConsole.Entities
+{
+    public class ActorDbContext : DbContext
+    {
+        public DbSet<Actor> Actors { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionBuilder)
+        {
+            optionBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=ActorDb;Trusted_Connection=True");
+        }
+    }
+}
